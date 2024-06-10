8f9dc508e1ab103439ab89677bb7a3fa9edd5e96
minhdang0603 <ruabin1163@gmail.com>
2024-06-10 16:27:01 +0700

    first commit

---
+++ DataAccess/Repository/BookingDetailRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObject;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class BookingDetailRepository : IBookingDetailRepository
+    {
+        public void AddBookingDetail(BookingDetail bookingDetail) => BookingDetailDAO.Instance.Add(bookingDetail);
+
+        public List<BookingDetail> GetAll() => BookingDetailDAO.Instance.GetBookingDetailsList().ToList();
+
+        public BookingDetail GetByBookingIdAndRoomID(int bookingID, int roomID) => BookingDetailDAO.Instance.GetBookingDetailsByBookingReservationIDAndRoomID(bookingID, roomID);
+
+        public void RemoveBookingDetail(BookingDetail bookingDetail) => BookingDetailDAO.Instance.Delete(bookingDetail);
+        public void UpdateBookingDetail(BookingDetail bookingDetail) => BookingDetailDAO.Instance.Update(bookingDetail);    
+    }
+}
---
+++ DataAccess/Repository/BookingReservationRepository.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObject;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class BookingReservationRepository : IBookingReservationRepository
+    {
+        public void AddBooking(BookingReservation bookingReservation) => BookingReservationDAO.Instance.Add(bookingReservation);
+
+        public List<BookingReservation> GetAll() => BookingReservationDAO.Instance.GetBookingReservationsList().ToList();
+
+        public BookingReservation GetByID(int id) => BookingReservationDAO.Instance.GetBookingReservationByID(id);
+
+        public void RemoveBooking(BookingReservation bookingReservation) => BookingReservationDAO.Instance.Delete(bookingReservation);
+
+        public void UpdateBooking(BookingReservation bookingReservation) => BookingReservationDAO.Instance.Update(bookingReservation);
+    }
+}
